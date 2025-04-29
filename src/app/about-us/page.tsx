8046aba1695050c8be0b254291bc1bc4ee167aab
sepehrsharafi <sepehr.sharafi123@gmail.com>
2025-04-29 20:53:34 +0330

    Task: Developed the Hero section  and Our values of the about us page

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "../ui/about-us/hero-section";
+import OurValues from "../ui/about-us/our-values";
+
+export default function Page() {
+  return (
+    <main>
+      <HeroSection />
+      <OurValues />
+    </main>
+  );
+}
