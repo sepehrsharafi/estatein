5de51342c6912ad17bd8279857d92cad43361c7e
sepehrsharafi <sepehr.sharafi.123@gmail.com>
2025-05-08 00:10:52 +0330

    Task: developed the filter section for mobile

---
+++ src/app/ui/listings/hero-section.tsx
@@ -0,0 +1,16 @@
+export default function HeroSection() {
+  return (
+    <section className="flex flex-col  gap-5 mx-4 xl:mx-20 2xl:mx-40 my-6 xl:my-20">
+      <div className="flex flex-col gap-2 mb-4 xl:mb-10 2xl:mb-12">
+        <h1 className="text-white text-3xl font-semibold xl:text-4xl 2xl:text-5xl">
+          Navigating the Estatein Experience
+        </h1>
+        <h2 className="text-neutral-400 text-sm xl:text-base 2xl:text-lg font-medium">
+          At Estatein, we've designed a straightforward process to help you find
+          and purchase your dream property with ease. Here's a step-by-step
+          guide to how it all works.
+        </h2>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/ui/listings/search-item.tsx
@@ -0,0 +1,33 @@
+export default function SearchItem({
+  icon,
+  placeholder,
+}: {
+  icon: string;
+  placeholder: string;
+}) {
+  return (
+    <div className="w-full flex flex-row justify-between items-center outline outline-[#262626] rounded-lg bg-[#141414] text-neutral-400 text-sm font-medium p-3.5">
+      <div className="flex flex-row gap-2">
+        <img className="w-5 h-5" src={icon} alt="icon" />
+        <div className="w-0.5 py-2.5 my-auto bg-[#262626]" />
+        <span>{placeholder}</span>
+      </div>
+      <div className="bg-[#1A1A1A] p-1.5 rounded-full">
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M16.25 6.875L10 13.125L3.75 6.875"
+            stroke="white"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+}
