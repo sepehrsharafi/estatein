14de4ec67127a597aba8cdf7a5074dfd20bfae66
sepehrsharafi <sepehr.sharafi.123@gmail.com>
2025-05-17 20:01:37 +0330

    Task: Developed the Image Gallery but it is unfinished

--- src/app/property/page.tsx
+++ src/app/property/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import HeroSection from "../ui/property/hero-section";
 
 export default function Page() {
-  return <main></main>;
+  return (
+    <main className="mx-4 xl:mx-20 2xl:mx-40 my-6 xl:my-20">
+      <HeroSection />
+    </main>
+  );
 }
